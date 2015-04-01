a117c77c7a6996cb6cc02bf8a9e230997b3874f1
Vincent <vincent.hagen@student.uva.nl>
2015-04-01 15:14:07 +0200

    Added timers to the unit test

--- HybreDb/HybreDbTest/UnitTest1.cs
+++ HybreDb/HybreDbTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using HybreDb.BPlusTree;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,34 +10,43 @@ namespace HybreDbTest {
         private Tree<int> Tree;
 
         public TreeTest() {
-            Tree = new Tree<int>(5);
+            Tree = new Tree<int>(50);
         }
 
         [TestMethod]
         public void TestInserts() {
-            int n = 1000;
+            int n = 10000;
 
             var rnd = new Random();
             var nums = new List<int>();
 
             while (nums.Count < n) {
                 int k = rnd.Next();
-                if (nums.Contains(k)) continue;
-                Tree.Insert(k, k);
+                if (nums.Contains(k)) continue;                
                 nums.Add(k);
             }
+
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (int t in nums)
+                Tree.Insert(t, t);
+            sw.Stop();
+            Trace.WriteLine("Insert took " + sw.ElapsedMilliseconds + "ms");
 
             CheckTree();
         }
 
 
         public void CheckTree() {
-
+            var sw = new Stopwatch();
+            sw.Start();
             int prev = int.MinValue;
             foreach (var v in Tree) {
                 Assert.IsFalse(v < prev, "Invalid tree");
                 prev = v;
             }
+            sw.Stop();
+            Trace.WriteLine("CheckTree took " + sw.ElapsedMilliseconds + "ms");
         }
     }
 }
