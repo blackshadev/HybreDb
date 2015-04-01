f4cef2efa8c14dbecb37a8469c19489d4d01c7cd
Vincent <vincent.hagen@student.uva.nl>
2015-04-01 14:58:56 +0200

    Added unit test

---
+++ HybreDb/HybreDbTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using HybreDb.BPlusTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HybreDbTest {
+    [TestClass]
+    public class TreeTest {
+        private Tree<int> Tree;
+
+        public TreeTest() {
+            Tree = new Tree<int>(5);
+        }
+
+        [TestMethod]
+        public void TestInserts() {
+            int n = 1000;
+
+            var rnd = new Random();
+            var nums = new List<int>();
+
+            while (nums.Count < n) {
+                int k = rnd.Next();
+                if (nums.Contains(k)) continue;
+                Tree.Insert(k, k);
+                nums.Add(k);
+            }
+
+            CheckTree();
+        }
+
+
+        public void CheckTree() {
+
+            int prev = int.MinValue;
+            foreach (var v in Tree) {
+                Assert.IsFalse(v < prev, "Invalid tree");
+                prev = v;
+            }
+        }
+    }
+}
