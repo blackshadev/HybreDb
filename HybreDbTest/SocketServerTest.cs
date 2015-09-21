139e7638cf959af36830e3953af3ea67d7b5cc47
Vincent <vinnie@script4web.nl>
2015-09-21 13:36:58 +0200

    Trying other port in the test case

--- HybreDbTest/SocketServerTest.cs
+++ HybreDbTest/SocketServerTest.cs
@@ -7,15 +7,17 @@ using NUnit.Framework;
 
 namespace HybreDbTest {
     public class SocketServerTest {
+        private const int Port = 4321;
+
         [TestCase]
         public void Small() {
-            var s = new ThreadedSocketServer();
+            var s = new ThreadedSocketServer(Port);
             s.OnDataReceived += (o, e) => Console.WriteLine(e.Message);
             s.Start();
 
-            Send(4242,
+            Send(Port,
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius auctor erat id suscipit. Nulla eget imperdiet lectus, vitae consequat mi. Proin sollicitudin condimentum nunc, ac imperdiet dolor sodales eu. Nunc non dui quis arcu tincidunt imperdiet non sed risus. Donec rutrum scelerisque tellus eget vestibulum. Praesent lacus risus, tristique et vulputate sit amet, dignissim id dui. Integer at venenatis odio, sed euismod ipsum. Nunc suscipit tempor diam, eu porttitor nunc vehicula at. Donec mauris velit, venenatis ut ligula eu, commodo eleifend odio. Mauris molestie vitae lacus a consectetur. Nullam interdum aliquam mi et placerat. Nullam et augue nec nisi laoreet bibendum sed at ante. Pellentesque semper nec leo varius porta. Nullam fringilla rhoncus orci congue tristique. Donec at arcu dui. Maecenas non mi ultrices, vestibulum diam at, vulputate augue");
-            Send(4242,
+            Send(Port,
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius auctor erat id suscipit. Nulla eget imperdiet lectus, vitae consequat mi. Proin sollicitudin condimentum nunc, ac imperdiet dolor sodales eu. Nunc non dui quis arcu tincidunt imperdiet non sed risus. Donec rutrum scelerisque tellus eget vestibulum. Praesent lacus risus, tristique et vulputate sit amet, dignissim id dui. Integer at venenatis odio, sed euismod ipsum. Nunc suscipit tempor diam, eu porttitor nunc vehicula at. Donec mauris velit, venenatis ut ligula eu, commodo eleifend odio. Mauris molestie vitae lacus a consectetur. Nullam interdum aliquam mi et placerat. Nullam et augue nec nisi laoreet bibendum sed at ante. Pellentesque semper nec leo varius porta. Nullam fringilla rhoncus orci congue tristique. Donec at arcu dui. Maecenas non mi ultrices, vestibulum diam at, vulputate augue");
 
             // Sleep to make sure the messages are received by the server
