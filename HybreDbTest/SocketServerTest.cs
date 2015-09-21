640646a87b58f7ba3aeab55e527d64257714d588
Vincent <vinnie@script4web.nl>
2015-09-21 13:40:15 +0200

    Increased wait time in the socket server test (temporarty fix)

--- HybreDbTest/SocketServerTest.cs
+++ HybreDbTest/SocketServerTest.cs
@@ -21,7 +21,7 @@ namespace HybreDbTest {
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius auctor erat id suscipit. Nulla eget imperdiet lectus, vitae consequat mi. Proin sollicitudin condimentum nunc, ac imperdiet dolor sodales eu. Nunc non dui quis arcu tincidunt imperdiet non sed risus. Donec rutrum scelerisque tellus eget vestibulum. Praesent lacus risus, tristique et vulputate sit amet, dignissim id dui. Integer at venenatis odio, sed euismod ipsum. Nunc suscipit tempor diam, eu porttitor nunc vehicula at. Donec mauris velit, venenatis ut ligula eu, commodo eleifend odio. Mauris molestie vitae lacus a consectetur. Nullam interdum aliquam mi et placerat. Nullam et augue nec nisi laoreet bibendum sed at ante. Pellentesque semper nec leo varius porta. Nullam fringilla rhoncus orci congue tristique. Donec at arcu dui. Maecenas non mi ultrices, vestibulum diam at, vulputate augue");
 
             // Sleep to make sure the messages are received by the server
-            Thread.Sleep(100);
+            Thread.Sleep(1000);
 
             s.Stop();
         }
